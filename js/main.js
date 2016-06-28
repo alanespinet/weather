3fdfee607ddc707066f57be06ed2e6be229e9fc5
alanespinet <aelluvetaa@gmail.com>
2016-06-27 21:50:43 -0400

    finished project

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ $.ajax({
     $.ajax({
       url: "http://api.openweathermap.org/data/2.5/weather?zip=" + resp.postal + ",us&appid=d82584a521ae673b1d3d837008607159",
       success: function(innerresp, innertxt, innerxhr){
-        var ans = "mininum temperature: " + innerresp.main.temp_min + "<br />" +
-                  "maximun temperature: " + innerresp.main.temp_max + "<br />" +
-                  "humidity: " + innerresp.main.humidity;
+        var ans = "The Mininum Temperature is: <span class='value'>" + innerresp.main.temp_min + "</span><br />" +
+                  "The Maximun Temperature is: <span class='value'>" + innerresp.main.temp_max + "</span><br />" +
+                  "The Humidity average is: <span class='value'>" + innerresp.main.humidity + "%</span>";
         $("#weather").html(ans);
       }
     });
